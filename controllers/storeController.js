44a58cb53110092f2085add32296b42aee810f73
Paul Coffey <pcoffey2@gmail.com>
2018-09-26 23:10:28 +0900

    Created user accounts authentication

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -46,7 +46,7 @@ exports.resize = async (req, res, next) => {
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
-    res.redirect(`/stores/${store.slug}`);
+    res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 
 router.post('/add', 
     storeController.upload, 
@@ -26,5 +28,20 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
+router.get('/register', userController.registerForm);
+
+// validate the registration data
+// register the user
+// need to log them in
+router.post('/register', 
+    userController.validateRegister,
+    userController.register,
+    authController.login
+);
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
